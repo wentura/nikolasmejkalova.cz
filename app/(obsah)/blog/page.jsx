1ff316fa787bb642df0d728129a46bff5b5cb67c
Zbynek Svoboda <wentura17@seznam.cz>
2024-04-14 15:53:28 +0200

    blog asi jede

--- app/(obsah)/blog/page.jsx
+++ app/(obsah)/blog/page.jsx
@@ -5,46 +5,102 @@ import Sluzby from "@/components/sluzby";
 import { gql, useMutation, useQuery } from "@apollo/client";
 import React from "react";
 
+// const GET_NABIDKA = gql`
+//   query GetPosts {
+//     posts(where: { categoryName: "blog" }) {
+//       nodes {
+//         id
+//         content
+//         date
+//         featuredImage {
+//           node {
+//             altText
+//             sourceUrl
+//           }
+//         }
+//         id
+//         title
+//         ytEmbed {
+//           ytEmbed
+//         }
+//         perex {
+//           perex
+//         }
+//       }
+//     }
+//   }
+// `;
+
 const GET_NABIDKA = gql`
   query GetPosts {
     posts(where: { categoryName: "blog" }) {
       nodes {
         id
-        content
         date
-        featuredImageId
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+          }
+        }
+        id
         title
-        ytEmbed {
-          ytEmbed
+        perex {
+          perex
         }
       }
     }
   }
 `;
 
-export default function Terapie() {
+export default function Blog() {
   const { loading, error, data, refetch } = useQuery(GET_NABIDKA);
 
   return (
     <div className="mx-auto max-w-screen-xl px-4 md:px-8 bg-white py-6 sm:py-8 lg:py-12">
-      <div className="divide-y divide-neutral-200">
+      <div className="">
         {error && <p>Error: {error.message}</p>}
-        {loading && <p>Načítám aktuální nabídku z Biozahájí...</p>}
+        {loading && <p>Načítám blog...</p>}
         {data &&
           data.posts.nodes.map((post) => (
             <div
-              className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0"
+              className="blogList flex flex-col md:flex-row pb-16"
               key={post.id}
             >
-              <h3 className="font-semibold md:col-span-3">{post.title}</h3>
-              <span
-                className="md:pl-0 md:col-span-7"
-                dangerouslySetInnerHTML={{ __html: post.content }}
-              ></span>
-              {/* <span
-                className="md:pl-0 md:col-span-2"
-                dangerouslySetInnerHTML={{ __html: post.cena.cena }}
-              ></span> */}
+              <div className="bg-white py-6 sm:py-8 lg:py-12">
+                <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+                  <div className="grid gap-8 md:grid-cols-2 lg:gap-12">
+                    <div className="h-64 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-auto">
+                      <img
+                        src={`${post.featuredImage.node.sourceUrl}`}
+                        loading="lazy"
+                        alt={`${post.featuredImage.node.altText}`}
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
+
+                    <div className="md:pt-8">
+                      <h1 className="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6 md:text-left">
+                        <a
+                          href={`/blogPosts/${post.id}`}
+                          className="underline underline-offset-8 decoration-gray-300 decoration-1 hover:decoration-gray-700 hover:decoration-3 transition duration-300"
+                        >
+                          {post.title}
+                        </a>
+                      </h1>
+
+                      <p
+                        className="mb-6 text-gray-500 sm:text-lg md:mb-8"
+                        dangerouslySetInnerHTML={{ __html: post.perex.perex }}
+                      ></p>
+
+                      <div className="readMore text-right text-sm underline underline-offset-4 decoration-gray-300 decoration-1 hover:decoration-gray-700 hover:decoration-3 transition duration-300 pt-4 md:pt-12">
+                        <a href={`/blogPosts/${post.id}`}>číst příspěvek</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
       </div>
---
+++ app/(obsah)/blogPosts/[id]/page.jsx
@@ -0,0 +1,52 @@
+"use client";
+import Heading from "@/components/heading";
+import How from "@/components/how";
+import Sluzby from "@/components/sluzby";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import React from "react";
+
+export default function BlogPost({ params }) {
+  const titId = params.id;
+
+  const GET_BLOGPOST = gql`
+    query getPost {
+      post(id: "${titId}") {
+          id
+          title
+          content
+          ytEmbed {
+      ytEmbed
+    }
+      }
+    }
+  `;
+  const { loading, error, data, refetch } = useQuery(GET_BLOGPOST, {
+    variables: { titId },
+  });
+  if (loading)
+    return (
+      <div className="mx-auto max-w-screen-xl px-4 md:px-8 bg-white py-6 sm:py-8 lg:py-12">
+        <p className="w-full text-center">Načítám článek z databáze.</p>
+      </div>
+    );
+  if (error)
+    return (
+      <div className="mx-auto max-w-screen-xl px-4 md:px-8 bg-white py-6 sm:py-8 lg:py-12">
+        <p>Error - ${error.message}</p>
+      </div>
+    );
+  return (
+    <div className="mx-auto max-w-screen-xl px-4 md:px-8 bg-white py-6 sm:py-8 lg:py-12">
+      <div className="bg-white py-6 sm:py-8 lg:py-12">
+        <h1 className="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6">
+          {data.post.title}
+        </h1>
+
+        <div
+          className="mb-6 text-gray-500 sm:text-lg md:mb-8 blogPost"
+          dangerouslySetInnerHTML={{ __html: data.post.content }}
+        ></div>
+      </div>
+    </div>
+  );
+}
