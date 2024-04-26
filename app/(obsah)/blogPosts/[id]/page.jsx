b6f1b9f2e3f938f6507d96096831ea4632127758
Zbynek Svoboda <wentura17@seznam.cz>
2024-04-26 11:05:51 +0200

    ytEmbed a responsivness

--- app/(obsah)/blogPosts/[id]/page.jsx
+++ app/(obsah)/blogPosts/[id]/page.jsx
@@ -35,17 +35,23 @@ export default function BlogPost({ params }) {
         <p>Error - ${error.message}</p>
       </div>
     );
+  const embed = data.post.ytEmbed;
   return (
     <div className="mx-auto max-w-screen-xl px-4 md:px-8 bg-white py-6 sm:py-8 lg:py-12">
       <div className="bg-white py-6 sm:py-8 lg:py-12">
         <h1 className="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6">
           {data.post.title}
         </h1>
-
         <div
           className="mb-6 text-gray-500 sm:text-lg md:mb-8 blogPost"
           dangerouslySetInnerHTML={{ __html: data.post.content }}
         ></div>
+        {!embed.ytEmbed || (
+          <div
+            className="my-12 text-gray-500 sm:text-lg md:mb-8 blogPost youtube-video-container"
+            dangerouslySetInnerHTML={{ __html: embed.ytEmbed }}
+          ></div>
+        )}
       </div>
     </div>
   );
