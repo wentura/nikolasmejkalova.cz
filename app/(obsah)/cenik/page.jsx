da27a695288b1a001c631d0412fd9024f1e455a3
Zbynek Svoboda <wentura17@seznam.cz>
2024-07-23 08:04:45 +0200

    nova cena 2000 za sezeni

--- app/(obsah)/cenik/page.jsx
+++ app/(obsah)/cenik/page.jsx
@@ -3,23 +3,23 @@ import React from "react";
 
 export default function Cenik() {
   return (
-    <div className="bg-white py-6 sm:py-8 lg:py-12">
-      <div className="mx-auto max-w-screen-xl px-4 md:px-8">
-        <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl xl:mb-12">
+    <div className="py-6 bg-white sm:py-8 lg:py-12">
+      <div className="max-w-screen-xl px-4 mx-auto md:px-8">
+        <h2 className="mb-4 text-2xl font-bold text-center text-gray-800 md:mb-8 lg:text-3xl xl:mb-12">
           Ceny tarapeutických sezení
         </h2>
 
-        <div className="mb-6 grid gap-x-6 gap-y-12 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 lg:gap-x-8">
+        <div className="grid mb-6 gap-x-6 gap-y-12 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 lg:gap-x-8">
           {/* plan - start */}
           <div className="flex flex-col space-y-4">
-            <div className="flex flex-col gap-4 rounded-lg border p-4 pt-6">
-              <h3 className="text-center text-2xl font-semibold text-gray-800">
+            <div className="flex flex-col gap-4 p-4 pt-6 border rounded-lg">
+              <h3 className="text-2xl font-semibold text-center text-gray-800">
                 Jedno sezení
               </h3>
 
               <div className="flex items-end justify-center gap-1">
                 <span className="text-4xl font-bold text-gray-800">
-                  1500 Kč
+                  2000 Kč
                 </span>
               </div>
             </div>
@@ -28,48 +28,48 @@ export default function Cenik() {
 
           {/* plan - start */}
           <div className="flex flex-col space-y-4">
-            <div className="relative flex flex-col gap-4 rounded-lg border border-yellow-600 p-4 pt-6">
-              <div className="absolute inset-x-0 -top-3 flex justify-center">
-                <span className="flex h-6 items-center justify-center rounded-full bg-yellow-600 px-3 py-1 text-xs font-semibold uppercase tracking-widest text-white">
+            <div className="relative flex flex-col gap-4 p-4 pt-6 border border-yellow-600 rounded-lg">
+              <div className="absolute inset-x-0 flex justify-center -top-3">
+                <span className="flex items-center justify-center h-6 px-3 py-1 text-xs font-semibold tracking-widest text-white uppercase bg-yellow-600 rounded-full">
                   oblíbenné
                 </span>
               </div>
 
-              <h3 className="text-center text-2xl font-semibold text-gray-800">
+              <h3 className="text-2xl font-semibold text-center text-gray-800">
                 Tři sezení
               </h3>
 
               <div className="flex items-end justify-center gap-1">
                 <span className="text-4xl font-bold text-gray-800">
-                  4300 Kč
+                  5400 Kč
                 </span>
               </div>
 
-              <div className="mb-4 flex items-center justify-center gap-1 text-xs text-gray-500">
-                zvýhodněná cena 1430 Kč za sezení
+              <div className="flex items-center justify-center gap-1 mb-4 text-xs text-gray-500">
+                zvýhodněná cena 1800 Kč za sezení
               </div>
             </div>
           </div>
-          {/* plan - end */}
 
           {/* plan - start */}
           <div className="flex flex-col space-y-4">
-            <div className="flex flex-col gap-4 rounded-lg border p-4 pt-6">
-              <h3 className="text-center text-2xl font-semibold text-gray-800">
+            <div className="flex flex-col gap-4 p-4 pt-6 border rounded-lg">
+              <h3 className="text-2xl font-semibold text-center text-gray-800">
                 Pět sezení
               </h3>
 
               <div className="flex items-end justify-center gap-1">
                 <span className="text-4xl font-bold text-gray-800">
-                  7000 Kč
+                  7750 Kč
                 </span>
               </div>
 
-              <div className="mb-4 flex items-center justify-center gap-1 text-sm text-gray-500">
-                zvýhodněná cena 1400 Kč za sezení
+              <div className="flex items-center justify-center gap-1 mb-4 text-sm text-gray-500">
+                zvýhodněná cena 1550 Kč za sezení
               </div>
             </div>
           </div>
+          {/* plan - end */}
         </div>
       </div>
       <How />
